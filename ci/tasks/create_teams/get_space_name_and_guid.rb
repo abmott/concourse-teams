4782397b08e027cc15ec2ba22abcdfa4742fbe6f
abmott <adam.mott@csaa.com>
2018-06-26 15:14:01 -0700

    changed space list formula

--- ci/tasks/create_teams/get_space_name_and_guid.rb
+++ ci/tasks/create_teams/get_space_name_and_guid.rb
@@ -11,7 +11,8 @@ def get_space_name_and_guid(spaces)
       if k == "entity"
         if v['name'].include?("dev") || v['name'].include?("DEV")
           if !v['name'].include?("dev2")
-          spaces_and_guid.merge!("#{v['name']}": "#{v['apps_url']}".split("/v2/spaces/")[1].split("/apps")[0])
+            spaces_and_guid.merge!("#{v['name']}": "#{v['apps_url']}".split("/v2/spaces/")[1].split("/apps")[0])
+          end
         end
       end
     end
