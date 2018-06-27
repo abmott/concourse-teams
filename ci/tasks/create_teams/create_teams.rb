2fbe5abc8baa6ac8cbe4e6eaff5e4f98f771b9d1
abmott <adam.mott@csaa.com>
2018-06-27 13:56:22 -0700

    test team create

--- ci/tasks/create_teams/create_teams.rb
+++ ci/tasks/create_teams/create_teams.rb
@@ -48,6 +48,7 @@ orgs.each do |org|
   space_guid.each do |space, guid|
     space = space.to_s
     if space.include?("dev") || space.include?("DEV") || space.include?("Dev")
+      puts "make team"
       create_team("#{space}", "#{guid}", "#{c_env}", "#{client_id}", "#{client_secret}", "#{auth_url}", "#{token_url}", "#{cf_url}")
     end
   end
