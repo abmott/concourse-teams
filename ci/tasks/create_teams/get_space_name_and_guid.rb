ff564519504aeffb8db72a1d6c5866ba51e173c0
abmott <adam.mott@csaa.com>
2018-07-24 15:51:09 -0700

    updated for pages

--- ci/tasks/create_teams/get_space_name_and_guid.rb
+++ ci/tasks/create_teams/get_space_name_and_guid.rb
@@ -7,7 +7,8 @@ def get_space_name_and_guid(env_url, page_url, token)
   page = 1
   lastpage = 10
   puts page_url
-  page_url = page_url && "?order-direction=asc&results-per-page=100"
+  page_url = page_url + "?order-direction=asc&results-per-page=100"
+  puts page_url
   spaces_and_guid = Hash.new
   until page > lastpage
     data = `curl "#{env_url}#{page_url}" -X GET -H "Authorization: bearer #{token}" -k -s`
