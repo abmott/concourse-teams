2a0cd4324af7f498c0ca131d2c7951ef2c8892c9
abmott <adam.mott@csaa.com>
2018-09-06 14:30:09 -0700

    updaed create teams method

--- ci/tasks/create_teams/create_team_method.rb
+++ ci/tasks/create_teams/create_team_method.rb
@@ -4,14 +4,7 @@ def create_team(space, guid, c_env, client_id, client_secret, auth_url, token_ur
   wrkdir = Dir.pwd
   puts "Started creating concourse team #{space}"
   fly = `fly -t #{c_env} set-team -n "#{space}" \
-  --non-interactive \
-  --uaa-auth-client-id #{client_id} \
-  --uaa-auth-client-secret '#{client_secret}' \
-  --uaa-auth-auth-url #{auth_url} \
-  --uaa-auth-token-url #{token_url} \
-  --uaa-auth-cf-url #{cf_url} \
-  --uaa-auth-cf-ca-cert #{wrkdir}/wildcard.cer \
-  --uaa-auth-cf-space #{guid}`
+  --cf-space #{org}:#{space}
   puts "Finished creating concourse team #{space}"
   puts "--------------------------------------------------"
 end
